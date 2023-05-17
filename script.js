88daff969d834d4ccb9885c2932f8843bb88cf30
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-05-17 20:29:50 +0200

    - Create a game() function that plays 5 rounds, logs the result of each round, keeps count of the score and declares a winner at the end - Declare draw, playerChoice and ComputerChoice variables. - Change the logic of the playOneRound function to return true in case of player victory or draw and return false in case of player loss. In case of draw, update the draw variable. Always update the playerChoice and computerChoice variables so that the game function can have access to them.

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
 "use strict"
+let playerChoice;
+let computerChoice;
+let draw;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 100);
-    return randomNumber > 66? "scissors" : randomNumber <= 33 ? "rock" : "paper";
+    computerChoice = randomNumber > 66? "scissors" : randomNumber <= 33 ? "rock" : "paper";
+    return computerChoice;
 }
 
 function getPlayerChoice(){
-    return prompt("?");
+    playerChoice = prompt("?");
+    return playerChoice;
 }
 
 function playOneRound(computerChoice, playerChoice){
-    let victory;
     if (computerChoice == playerChoice){
-        return "Draw"
+        draw = true;
+        return true;
     } else if (playerChoice == "rock" && computerChoice == "scissors") {
-        victory = true;
+        draw = false;
+        return true;
     } else if (playerChoice == "paper" && computerChoice == "rock"){
-        victory = true;
+        draw = false;
+        return true;
     } else if (playerChoice == "scissors" && computerChoice == "paper"){
-        victory = true;
+        draw = false;
+        return true;
     } else {
-        victory = false;
-    }
-
-    if (victory === true){
-        return "You win! " + `${playerChoice} beats ${computerChoice}!`
-    } else {
-        return "You lose! " + `${computerChoice} beats ${playerChoice}!`
+        draw = false;
+        return false;
     }
 };
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let a = 0; a < 5; a++){
+        if (playOneRound(getComputerChoice(), getPlayerChoice())){
+            if (draw){
+                console.log("Draw")
+            } else {
+                console.log("You win! " + `${playerChoice} beats ${computerChoice}!`)
+                playerScore ++;
+            }
+        } else {
+            console.log("You lose! " + `${computerChoice} beats ${playerChoice}!`)
+            computerScore ++;
+        }
+    }
+    if (playerScore == computerScore){
+        console.log("It's a draw!");
+    } else if (playerScore > computerScore){
+        console.log("Player wins!");
+    } else {
+        console.log("Computer wins!");
+    }
+}
