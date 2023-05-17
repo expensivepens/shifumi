ac4005a8def570447cdcfd11a079522bb331510c
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-05-17 18:18:26 +0200

    Create playOneRound function that plays one round of rock paper scissors given two inputs and returns a string containing the result of the game

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 "use strict"
 
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-    let computerChoice = randomNumber > 66? "scissors" : randomNumber <= 33 ? "rock" : "paper";
-    return computerChoice;
+    let randomNumber = Math.floor(Math.random() * 100);
+    return randomNumber > 66? "scissors" : randomNumber <= 33 ? "rock" : "paper";
 }
+
+
+function playOneRound(computerChoice, playerChoice){
+    let victory;
+    if (computerChoice == playerChoice){
+        return "Draw"
+    } else if (playerChoice == "rock" && computerChoice == "scissors") {
+        victory = true;
+    } else if (playerChoice == "paper" && computerChoice == "rock"){
+        victory = true;
+    } else if (playerChoice == "scissors" && computerChoice == "paper"){
+        victory = true;
+    } else {
+        victory = false;
+    }
+
+    if (victory === true){
+        return "You win! " + `${playerChoice} beats ${computerChoice}!`
+    } else {
+        return "You lose! " + `${computerChoice} beats ${playerChoice}!`
+    }
+};
